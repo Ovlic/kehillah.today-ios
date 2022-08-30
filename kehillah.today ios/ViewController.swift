4faadfed6eb4886d62bfa84566fabed303a00287
Ovlic <74332950+Ovlic@users.noreply.github.com>
2022-08-30 10:37:49 -0700

    Initial Commit

--- kehillah.today ios/ViewController.swift
+++ kehillah.today ios/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var WebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //var URL = URL(string: "https://kehillah.today")!
+        //WebView.load(URLRequest(url: URL))
+        
+        WebView.load(URLRequest(url: URL(fileURLWithPath: Bundle.main.path(forResource: "kehillah_today", ofType: "html")!)))
+        
     }
 
 
